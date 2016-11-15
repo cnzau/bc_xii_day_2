5f4a6832b3e59757f3cf593d210492766b55d411
cnzau <clement.nzau@gmail.com>
2016-11-15 10:36:33 +0300

    dynamic user name made by user

--- http_web/github_repos.py
+++ http_web/github_repos.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 import requests
+print("Console app to print github repositories and languages used")
 
-response = requests.get('https://api.github.com/users/cnzau/repos')
+user = input("Enter your GitHub Username: ")
+
+if user == None:
+	print("User not provided!")
+	
+response = requests.get('https://api.github.com/users/%s/repos' % user)
 
 assert response.status_code == 200
 
 for repo in response.json():
-    print (repo['name'], '\t\t\t\t', repo['language'])
+    print(repo['name'], '\t\t\t\t', repo['language'])
